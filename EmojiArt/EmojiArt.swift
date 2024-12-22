f33ff53af638c6139e90d09e6c40f68b97fb7321
Jakub Malinkiewicz <me@jmalinkiewicz.pl>
2024-12-22 01:57:29 +0100

    add subscripts to EmojiArt

--- EmojiArt/EmojiArt.swift
+++ EmojiArt/EmojiArt.swift
@@ -17,6 +17,33 @@ struct EmojiArt {
         uniqueEmojiId += 1
         emojis.append(Emoji(string: emoji, position: position, size: size, id: uniqueEmojiId))
     }
+
+    subscript(_ emojiId: Emoji.ID) -> Emoji? {
+        if let index = index(of: emojiId) {
+            return emojis[index]
+        } else {
+            return nil
+        }
+    }
+
+    subscript(_ emoji: Emoji) -> Emoji {
+        get {
+            if let index = index(of: emoji.id) {
+                return emojis[index]
+            } else {
+                return emoji // should probably throw error
+            }
+        }
+        set {
+            if let index = index(of: emoji.id) {
+                emojis[index] = newValue
+            }
+        }
+    }
+    
+    private func index(of emojiId: Emoji.ID) -> Int? {
+        emojis.firstIndex(where: { $0.id == emojiId })
+    }
     
     struct Emoji: Identifiable {
         let string: String
